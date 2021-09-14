a81cac48d712e9c2d093423be8c81bba01f7dd3c
ugjka <esesmu@protonmail.com>
2021-09-14 19:56:43 +0300

    herp derp

--- _site/sw.js
+++ _site/sw.js
@@ -14,17 +14,13 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v15';
+const PRECACHE = 'precache-v16';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    './',
-    './index.html',
     './background.png',
-    './elements.js',
     './favicon-192x192.png',
-    './manifest.webmanifest',
     './slim-js/index.js',
     './slim-js/decorators.js',
     './slim-js/directives/all.js'
